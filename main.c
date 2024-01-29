02c7819f2a744c75eb7ee5c48240012c53081f70
Mohammad Fallahinezhad <fallahi.m1383@gmail.com>
2024-01-29 23:38:36 +0330

    fixed a bug in config

--- main.c
+++ main.c
@@ -12,9 +12,17 @@ int main(int argc, char *argv[]) {
     if(!strcmp(argv[1], "config")) {
         int error;
         if(strcmp(argv[2], "-global")) {
-            error = config(argv[2], argv[3], 0);
+            if(strcmp(argv[2], "user.name") && strcmp(argv[2], "user.email")) {
+                printf("invalid command\n");
+                error = 1;
+            }
+            else error = config(argv[2], argv[3], 0);
         } else {
-            error = config(argv[3], argv[4], 1);
+            if(strcmp(argv[3], "user.name") && strcmp(argv[3], "user.email")) {
+                printf("invalid command\n");
+                error = 1;
+            }
+            else error = config(argv[3], argv[4], 1);
         }
         if(error) {
             printf("an error occured.\n");
