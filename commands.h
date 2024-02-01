2fb28e3a9be5f85f5f1ec39cdc4d7d12a6808724
Mohammad Fallahinezhad <fallahi.m1383@gmail.com>
2024-02-01 16:03:57 +0330

    added basics for 'add' command

--- commands.h
+++ commands.h
@@ -4,35 +4,60 @@
 #include<string.h>
 #include<dirent.h>
 #include<windows.h>
+#include<sys/stat.h>
 #include<unistd.h>
+
+int check_initial_dir_existence() {
+    char current_path[500];
+    getcwd(current_path, sizeof(current_path));
+    int found = 0, root = 0;
+    do {
+        root = 1;
+        DIR* cwd = opendir(".");
+        struct dirent* entry;
+        while(entry = readdir(cwd)) {
+            if(!strcmp(entry->d_name, ".vics")) {
+                found = 1; break;
+            }
+            if(!strcmp(entry->d_name, "..")) root = 0;
+        }
+        closedir(cwd);
+        chdir("..");
+        if(found) break;
+        
+    } while(!root);
+    chdir(current_path);
+    return found;
+}
+
+void get_vics_folder(char* buffer) {
+    char current_path[500];
+    getcwd(current_path, sizeof(current_path));
+    int found = 0, root = 0;
+    do {
+        root = 1;
+        DIR* cwd = opendir(".");
+        struct dirent* entry;
+        while(entry = readdir(cwd)) {
+            if(!strcmp(entry->d_name, ".vics")) {
+                found = 1; break;
+            }
+            if(!strcmp(entry->d_name, "..")) root = 0;
+        }
+        closedir(cwd);
+        chdir("..");
+        if(found) break;
+        strcat(buffer, "../");
+    } while(!root);
+    chdir(current_path);
+    strcat(buffer, ".vics");
+}
 
 int config(char* info, char* value, int global) {
     char main_file_path[500], tmp_file_path[500];
     if(!global) {
         char vics_folder_path[500] = "";
-
-        char current_path[500];
-        getcwd(current_path, sizeof(current_path));
-        int found = 0, root = 0;
-        do {
-            root = 1;
-            DIR* cwd = opendir(".");
-            struct dirent* entry;
-            while(entry = readdir(cwd)) {
-                if(!strcmp(entry->d_name, ".vics")) {
-                    found = 1; break;
-                }
-                if(!strcmp(entry->d_name, "..")) root = 0;
-            }
-            closedir(cwd);
-            chdir("..");
-            if(found) break;
-            strcat(vics_folder_path, "../");
-
-        } while(!root);
-        chdir(current_path);
-
-        strcat(vics_folder_path, ".vics");
+        get_vics_folder(vics_folder_path);
 
         strcpy(main_file_path, vics_folder_path); strcat(main_file_path, "/user-info.txt");
         strcpy(tmp_file_path, vics_folder_path); strcat(tmp_file_path, "/tmp.txt");
@@ -82,29 +107,7 @@ int add_alias(char* info, char* value, int global) {
     char main_file_path[500], tmp_file_path[500];
     if(!global) {
         char vics_folder_path[500] = "";
-
-        char current_path[500];
-        getcwd(current_path, sizeof(current_path));
-        int found = 0, root = 0;
-        do {
-            root = 1;
-            DIR* cwd = opendir(".");
-            struct dirent* entry;
-            while(entry = readdir(cwd)) {
-                if(!strcmp(entry->d_name, ".vics")) {
-                    found = 1; break;
-                }
-                if(!strcmp(entry->d_name, "..")) root = 0;
-            }
-            closedir(cwd);
-            chdir("..");
-            if(found) break;
-            strcat(vics_folder_path, "../");
-
-        } while(!root);
-        chdir(current_path);
-
-        strcat(vics_folder_path, ".vics");
+        get_vics_folder(vics_folder_path);
 
         strcpy(main_file_path, vics_folder_path); strcat(main_file_path, "/aliases.txt");
         strcpy(tmp_file_path, vics_folder_path); strcat(tmp_file_path, "/tmp.txt");
@@ -150,36 +153,72 @@ int add_alias(char* info, char* value, int global) {
     return 0;
 }
 
-int check_initial_dir_existence() {
-    char current_path[500];
-    getcwd(current_path, sizeof(current_path));
-    int found = 0, root = 0;
-    do {
-        root = 1;
-        DIR* cwd = opendir(".");
-        struct dirent* entry;
-        while(entry = readdir(cwd)) {
-            if(!strcmp(entry->d_name, ".vics")) {
-                found = 1; break;
-            }
-            if(!strcmp(entry->d_name, "..")) root = 0;
-        }
-        closedir(cwd);
-        chdir("..");
-        if(found) break;
-        
-    } while(!root);
-    chdir(current_path);
-    return found;
-}
-
 int init() {
     int error;
     error = check_initial_dir_existence();
     if(error) return 1;
     else {
+        // creating .vics:
         CreateDirectory(".vics", NULL);
         SetFileAttributes(".vics", FILE_ATTRIBUTE_HIDDEN);
+
+        // creating "staging" inside .vics:
+        chdir(".vics");
+        CreateDirectory("staging", NULL);
+        chdir("..");
+
         return 0;
     }
+}
+
+int check_equality(char* path1, char* path2) {
+    FILE* a = fopen(path1, "r");
+    FILE* b = fopen(path2, "r");
+    if(a == NULL || b == NULL) return 0;
+
+    char c1, c2;
+    int equal = 1;
+    do {
+        c1 = fgetc(a), c2 = fgetc(b);
+        if(c1 != c2) {
+            equal = 0; break;
+        }
+    } while(c1 != EOF && c2 != EOF);
+
+    fclose(a); fclose(b);
+    return equal;
+}
+
+void copy_file(char* path, char* dest) {
+    FILE* from = fopen(path, "r"); 
+    FILE* to = fopen(dest, "w");
+    char ch;
+    do {
+        ch = fgetc(from);
+        if(ch != EOF) fputc(ch, to);
+    } while(ch != EOF);
+    fclose(from); fclose(to);
+}
+
+int stage_file(char* path) {
+    char dest[500] = "";
+    char tmp[500] = ""; strcpy(tmp, path);
+    
+    strrev(tmp);
+    char* pos = strstr(tmp, "/");
+    if(pos == NULL) pos = strstr(tmp, "\\");
+    if(pos == NULL) {
+        strrev(tmp);
+    } else {
+        tmp[pos - tmp] = '\0';
+        strrev(tmp);
+    }
+
+    get_vics_folder(dest); strcat(dest, "/staging/"); strcat(dest, tmp);
+
+    int equal = check_equality(path, dest);
+    if(equal) return 0;
+
+    copy_file(path, dest);
+    return 1;
 }
--- main.c
+++ main.c
@@ -16,6 +16,19 @@ int validate_command(char* command) {
     return 0;
 }
 
+int is_file(char *path) {
+    struct stat s;
+    if (stat(path, &s) == 0) 
+    {
+        if (s.st_mode & S_IFDIR) 
+            return 0;
+        else if (s.st_mode & S_IFREG) 
+            return 1;
+    }
+    else
+        return -1;
+}
+
 int main(int argc, char *argv[]) {
 
     if(argc == 1) {
@@ -71,6 +84,34 @@ int main(int argc, char *argv[]) {
         int error = init();
         if(error) printf("you are inside a vics project and can not initialize another project.\n");
         else printf("welcome to ViCS!\n");
+
+    }
+
+    if(!strcmp(argv[1], "add")) {
+
+        for(int i = 2 + (!strcmp(argv[2], "-f")) ; i < argc ; i++) {
+            int result;
+            if(is_file(argv[i]) == 1) {
+                result = stage_file(argv[i]);
+                if(result) printf("%s: added successfully.\n", argv[i]);
+                else printf("%s: adding was unsuccessful.\n", argv[i]);
+            } else if(is_file(argv[i]) == 0){
+                DIR* dir = opendir(argv[i]);
+                char dir_path[500]; strcpy(dir_path, argv[i]);
+                int files_added = 0;
+                struct dirent* entry;
+                while(entry = readdir(dir)) {
+                    char file_path[500]; strcpy(file_path, dir_path); strcat(file_path, "/"); strcat(file_path, entry->d_name);
+                    if(is_file(file_path) == 1) {
+                        files_added += stage_file(file_path);
+                    }
+                }
+                closedir(dir);
+                printf("%s: %d files added successfully.\n", argv[i], files_added);
+            } else {
+                printf("%s: no such file or directory.\n", argv[i]);
+            }   
+        }
 
     }
 
